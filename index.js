4f8ec51bcd1837a206e7b9d3dc0654db1cc797b4
hishida <hiroshi.ishida@jp.ricoh.com>
2018-10-12 18:05:49 +0900

    updated log comment.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello DevOps Handson !!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000! ! !'))
 
 module.exports = app;
